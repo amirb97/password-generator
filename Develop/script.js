5eb8324baa9a4fada636f8fb937e8c9cd24c9039
Era <amirb2467@gmail.com>
2021-11-06 01:54:19 -0700

    funciton added to get desired password length from user, logic in place to determine what character types can be used

--- Develop/script.js
+++ Develop/script.js
@@ -1,56 +1,52 @@
 // Assignment code here
-
 // arrays to store each character type
-const nums = [
-  "0","1","2","3","4","5","6","7","8","9"
-];
-const lowerLetters = [
-  "a","b","c","d","e","f","g","h","i","j","k","l","m",
-  "n","o","p","q","r","s","t","u","v","w","x","y","z"
-];
-const upperLetters = [
-  "A","B","C","D","E","F","G","H","I","J","K","L","M",
-  "N","O","P","Q","R","S","T","U","V","W","X","Y","Z"
-];
-const specialChars = [
-  " ","!",'"',"#","$","%","&","'","(",")","*",
-  "+",",","-",".","/",":",";","<","=",">","?",
-  "@","[","]","^","_","`","{","|","}","~"
-];
+let nums = "0123456789";
+let lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+let upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let specialChars = '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~';
 
-var passLength = window.prompt("How many characters does the password need to be?(8-128 inclusive)");
+// string to be used to store all possible chars
+let possibleChars = "";
 
-// initializes 4 boolean variables
-let hasNums,hasLowerLetters,hasUpperLetters,hasSpecialChars;
+// initializes passLength as a global variable to be used in a function, and returned from that function
+let passLength;
+
+// function to get the desired password length from the user
+function passwordLength(){
+  passLength = window.prompt("How many characters does the password need to be?(8-128 inclusive)");
+
+  // if loop ensures the password will be a length within the given criteria
+  if(passLength >= 8 && passLength <= 128){
+    return passLength;
+  } else {
+    alert("You entered a numbers that was greater than 128 or less than 8.  Please enter a number between 8 and 128.");
+    passwordLength();
+  }
+}
+
+// calls the function to get the desired password length
+passwordLength();
 
 // if statements check if certain characters should be included in the password
 if(confirm('Should the password contain numbers?')){
-  hasNums = true;
-} else {
-  hasNums = false;
+  possibleChars = possibleChars.concat(nums);
 }
 
 if(confirm('Should the password contain lower case letters?')){
-  hasLowerLetters = true;
-} else {
-  hasLowerLetters = false;
+  possibleChars = possibleChars.concat(lowerLetters);
 }
 
 if(confirm('Should the password contain upper case letters?')){
-  hasUpperLetters = true;
-} else {
-  hasUpperLetters = false;
+  possibleChars = possibleChars.concat(upperLetters);
 }
 
 if(confirm('Should the password contain special characters?')){
-  hasSpecialChars = true;
-} else {
-  hasSpecialChars = false;
+  possibleChars = possibleChars.concat(specialChars);
 }
 
 // function to generate and return the password as a string
 function generatePassword(){
-  return hasNums;
+  return possibleChars;
 }
 
 // Get references to the #generate element
